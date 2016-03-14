3ae531123726834d0f3cb9872d60455ec70d7734
DraperStudio <brideloper@gmail.com>
2016-03-14 10:47:24 +0100

    Update AbstractTestCase.php

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -20,15 +20,5 @@ use GrahamCampbell\TestBench\AbstractPackageTestCase;
  */
 abstract class AbstractTestCase extends AbstractPackageTestCase
 {
-    /**
-     * Get the service provider class.
-     *
-     * @param \Illuminate\Contracts\Foundation\Application $app
-     *
-     * @return string
-     */
-    protected function getServiceProviderClass($app)
-    {
-        return \DraperStudio\Eloquent\Repositories\ServiceProvider::class;
-    }
+    //
 }
