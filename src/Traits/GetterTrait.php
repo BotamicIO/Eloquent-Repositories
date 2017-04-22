52d75e222e70d070be75af14ac4e848c92b25884
Brian Faust <hello@brianfaust.de>
2017-04-22 05:43:37 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Traits/GetterTrait.php
+++ src/Traits/GetterTrait.php
@@ -1,12 +1,18 @@
 <?php
 
 
-
 declare(strict_types=1);
 
-namespace BrianFaust\Eloquent\Repositories\Traits;
+/*
+ * This file is part of Eloquent Repositories.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-use BrianFaust\Eloquent\Repositories\Criteria\OrderBy;
+namespace BrianFaust\Eloquent\Repositories\Traits;
 
 trait GetterTrait
 {
